08a0e75eb82cd099c365bf4f751bcd69e5fa514a
griffinryan <glryan@uw.edu>
2024-03-04 10:51:59 -0800

    finished resume page, need to do 3D model now

---
+++ pages/resume.js
@@ -0,0 +1,37 @@
+import {Container, Heading, Divider, Badge, Box, Button} from "@chakra-ui/react";
+import Layout from "../components/layouts/article";
+import Section from "../components/section";
+import NextLink from "next/link";
+import { ChevronRightIcon, EmailIcon } from "@chakra-ui/icons";
+// maybe imports for tsParticles?????
+
+const Resume = () => (
+    <Layout title='Resume'>
+        <Container>
+
+            <Heading as ="h3" fontSize={20} mb={4}>
+                Resume
+            </Heading>
+            <Divider/>
+
+            <Section delay={0.2}>
+                <Box align ='center' my={4}>
+                    <Button
+                        as={NextLink}
+                        href="https://torpoisebucket.s3.us-west-2.amazonaws.com/Resume_GriffinRyan.pdf"
+                        scroll={false}
+                        rightIcon={<ChevronRightIcon />}
+                        colorScheme='teal'
+                    >
+                        Download Resume
+                    </Button>
+                </Box>
+            </Section>
+
+        </Container>
+    </Layout>
+
+)
+
+export default Resume
+export { getServerSideProps } from '../components/chakra'
